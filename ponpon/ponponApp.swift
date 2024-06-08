9f064d8368c4e664f911c1807381a823334c27c9
Nori <86976643+ybnr1130@users.noreply.github.com>
2024-06-08 15:48:01 +0900

    Initial Commit

---
+++ ponpon/ponponApp.swift
@@ -0,0 +1,17 @@
+//
+//  ponponApp.swift
+//  ponpon
+//
+//  Created by norihito on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct ponponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
